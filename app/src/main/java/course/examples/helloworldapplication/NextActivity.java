b716550db5f7666da11b70350bcf990aecae792f
RitaSusana <ritacoelhodasilva@gmail.com>
2016-09-28 22:23:47 +0100

    ADD BUTTON CLOSE AND CODE TO FINISH THE NEXTACTIVITY

--- app/src/main/java/course/examples/helloworldapplication/NextActivity.java
+++ app/src/main/java/course/examples/helloworldapplication/NextActivity.java
@@ -1,7 +1,10 @@
 package course.examples.helloworldapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Button closeButton = (Button) findViewById(R.id.btnClose);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                NextActivity.this.finish();
+            }
+        });
     }
 }
