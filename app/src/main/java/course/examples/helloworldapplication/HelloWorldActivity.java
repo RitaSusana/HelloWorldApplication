3f4655c37fbfd86cde060b713c9cf82c0fa5d779
RitaSusana <ritacoelhodasilva@gmail.com>
2016-09-28 21:35:06 +0100

    BTNNEXT CODE TO GO TO THE NEXT ACTIVITY

--- app/src/main/java/course/examples/helloworldapplication/HelloWorldActivity.java
+++ app/src/main/java/course/examples/helloworldapplication/HelloWorldActivity.java
@@ -1,7 +1,10 @@
 package course.examples.helloworldapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+
+        Button nextButton = (Button) findViewById(R.id.btnNext);
+        nextButton.SetOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                Intent i = new Intent(HelloWorldActivity.this, NextActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
